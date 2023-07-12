f0e5df47acc5130c4585e92468d7f0b0dcfddfd0
Judah <judahben149@gmail.com>
2023-07-12 15:48:29 +0100

    Set up logger and preferences utils

--- app/src/main/java/com/judahben149/emvsync/utils/LoggerUtils.kt
+++ app/src/main/java/com/judahben149/emvsync/utils/LoggerUtils.kt
@@ -2,11 +2,11 @@ package com.judahben149.emvsync.utils
 
 import timber.log.Timber
 
-fun String?.logThis() {
-    Timber.tag("TAG").d(this)
+fun String?.logThis(tag: String = "TAG") {
+    Timber.tag(tag).d(this)
 }
 
-fun Int?.logThis() {
-    Timber.tag("TAG").d(this.toString())
+fun Int?.logThis(tag: String = "TAG") {
+    Timber.tag(tag).d(this.toString())
 }
 
---
+++ app/src/main/java/com/judahben149/emvsync/utils/PreferencesHelper.kt
@@ -0,0 +1,9 @@
+package com.judahben149.emvsync.utils
+
+import android.content.SharedPreferences
+import javax.inject.Inject
+
+class PreferencesHelper @Inject constructor(sharedPrefs: SharedPreferences) {
+
+
+}
