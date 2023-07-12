31451c2b32a0081dc1cbdc501db12a030fe57cdf
Judah <judahben149@gmail.com>
2023-07-12 15:49:55 +0100

    Refactored package, set up lottie animation and state flow observer

--- app/src/main/java/com/judahben149/emvsync/presentation/keyExchange/KeyExchangeFragment.kt
+++ app/src/main/java/com/judahben149/emvsync/presentation/keyExchange/KeyExchangeFragment.kt
@@ -1,4 +1,4 @@
-package com.judahben149.emvsync.presentation.emv
+package com.judahben149.emvsync.presentation.keyExchange
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,8 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.judahben149.emvsync.databinding.FragmentKeyExchangeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class KeyExchangeFragment : Fragment() {
@@ -29,6 +34,16 @@ class KeyExchangeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel.doKeyExchange()
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.state.collect { state ->
+                    if (state.isPinKeyInjected) {
+                        binding.tvMasterKey.text = "Downloading keys successful"
+                    }
+                }
+            }
+        }
     }
 
     override fun onDestroy() {
