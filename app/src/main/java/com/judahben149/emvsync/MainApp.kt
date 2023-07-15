5ae97a7f9d4cf658caad724930fad9505416e955
Judah <judahben149@gmail.com>
2023-07-15 22:06:55 +0100

    Setup utils for injecting AIDs and CAPKs into terminal device on app startup

--- app/src/main/java/com/judahben149/emvsync/MainApp.kt
+++ app/src/main/java/com/judahben149/emvsync/MainApp.kt
@@ -1,11 +1,14 @@
 package com.judahben149.emvsync
 
 import android.app.Application
+import com.judahben149.emvsync.utils.emvUtils.EmvUtils
+import com.judahben149.emvsync.utils.logThis
 import com.nexgo.oaf.apiv3.APIProxy
 import com.nexgo.oaf.apiv3.DeviceEngine
 import com.nexgo.oaf.apiv3.emv.EmvHandler2
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
 class MainApp : Application() {
@@ -13,11 +16,16 @@ class MainApp : Application() {
     lateinit var deviceEngine: DeviceEngine
     lateinit var emvHandler2: EmvHandler2
 
+    @Inject
+    lateinit var emvUtils: EmvUtils
+
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
         deviceEngine = APIProxy.getDeviceEngine(this)
         emvHandler2 = deviceEngine.getEmvHandler2("app2")
 
+        emvUtils.initializeEmvAid(emvHandler2)
+        emvUtils.initializeEmvCapk(emvHandler2)
     }
 }
---
+++ app/src/main/java/com/judahben149/emvsync/utils/emvUtils/EmvUtils.kt
@@ -0,0 +1,103 @@
+package com.judahben149.emvsync.utils.emvUtils
+
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.JsonParser
+import com.judahben149.emvsync.utils.logThis
+import com.nexgo.oaf.apiv3.emv.AidEntity
+import com.nexgo.oaf.apiv3.emv.AidEntryModeEnum
+import com.nexgo.oaf.apiv3.emv.CapkEntity
+import com.nexgo.oaf.apiv3.emv.EmvHandler2
+import java.io.IOException
+import javax.inject.Inject
+
+class EmvUtils @Inject constructor(private val context: Context) {
+
+    fun getCapkList(): List<CapkEntity>? {
+        val capkList: MutableList<CapkEntity> = ArrayList()
+        val jsonArray = JsonParser.parseString(readAssetsText("emv_test_capk.json")).asJsonArray ?: return null
+
+        for(user in jsonArray) {
+            val userBean = Gson().fromJson(user, CapkEntity::class.java)
+            capkList.add(userBean)
+        }
+
+        return capkList
+    }
+
+    fun getAidList(): List<AidEntity>? {
+        val aidEntityList: MutableList<AidEntity> = ArrayList()
+        val jsonArray = JsonParser.parseString(readAssetsText("inbas_aid.json")).asJsonArray ?: return null
+
+        for(user in jsonArray) {
+            val userBean = Gson().fromJson(user, AidEntity::class.java)
+            val jsonObject = user.asJsonObject
+
+            if (jsonObject != null && jsonObject["emvEntryMode"] != null) {
+                val emvEntryMode = jsonObject["emvEntryMode"].asInt
+
+                userBean.aidEntryModeEnum = AidEntryModeEnum.values()[emvEntryMode]
+                "Emv entry mode - ${ userBean.aidEntryModeEnum }".logThis("Tagg")
+            }
+            aidEntityList.add(userBean)
+        }
+
+        return aidEntityList
+    }
+
+
+    fun initializeEmvAid(emvHandler2: EmvHandler2) {
+        emvHandler2.delAllAid()
+
+        if (emvHandler2.aidListNum <= 0) {
+            val emvAidList = getAidList()
+
+            if (emvAidList != null) {
+                val initResult = emvHandler2.setAidParaList(emvAidList)
+                "Init AID List result - $initResult".logThis("Tagg")
+            } else {
+                "Init AID List failed".logThis("Tagg")
+                return
+            }
+        } else {
+            "AID List already loaded".logThis("Tagg")
+        }
+    }
+
+    fun initializeEmvCapk(emvHandler2: EmvHandler2) {
+        emvHandler2.delAllCapk()
+
+        if (emvHandler2.capkListNum <= 0) {
+            val capkList = getCapkList()
+
+            if (capkList != null) {
+                val initResult = emvHandler2.setCAPKList(capkList)
+                "Init CAPK List result - $initResult".logThis("Tagg")
+            } else {
+                "Init CAPK List failed".logThis("Tagg")
+                return
+            }
+        } else {
+            "CAPK List already loaded".logThis("Tagg")
+        }
+    }
+
+    private fun readAssetsText(fileName: String): String? {
+
+        try {
+            val inputStream = context.assets.open(fileName)
+            val size = inputStream.available()
+
+            // Read the entire asset into a local byte buffer.
+            val buffer = ByteArray(size)
+            inputStream.read(buffer)
+            inputStream.close()
+
+            //convert the buffer into a string
+            return String(buffer)
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+        }
+        return null
+    }
+}
