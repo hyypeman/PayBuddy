30c46e164b3e9dc54ca68418810d92eee345b407
Judah <judahben149@gmail.com>
2023-07-19 23:26:40 +0100

    Refactored application class

--- app/src/main/java/com/judahben149/emvsync/MainApp.kt
+++ app/src/main/java/com/judahben149/emvsync/MainApp.kt
@@ -16,8 +16,8 @@ class MainApp : Application() {
     lateinit var deviceEngine: DeviceEngine
     lateinit var emvHandler2: EmvHandler2
 
-//    @Inject
-//    lateinit var emvUtils: EmvUtils
+    @Inject
+    lateinit var emvUtils: EmvUtils
 
     override fun onCreate() {
         super.onCreate()
@@ -25,7 +25,7 @@ class MainApp : Application() {
         deviceEngine = APIProxy.getDeviceEngine(this)
         emvHandler2 = deviceEngine.getEmvHandler2("app2")
 
-//        emvUtils.initializeEmvAid(emvHandler2)
-//        emvUtils.initializeEmvCapk(emvHandler2)
+        emvUtils.initializeEmvAid(emvHandler2)
+        emvUtils.initializeEmvCapk(emvHandler2)
     }
 }
