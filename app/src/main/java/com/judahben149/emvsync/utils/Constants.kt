0099e5998206c7182b89ca37cf1afc1a3360281d
Judah <judahben149@gmail.com>
2023-07-19 23:25:49 +0100

    Updated app constants

--- app/src/main/java/com/judahben149/emvsync/utils/Constants.kt
+++ app/src/main/java/com/judahben149/emvsync/utils/Constants.kt
@@ -4,6 +4,7 @@ object Constants {
 
     const val SETTINGS = "SETTINGS"
 
+    //Channel settings constants
     const val IP_ADDRESS = "196.6.103.18"
     const val PORT = "5001"
     const val TERMINAL_ID = "2070AL32"
@@ -12,10 +13,19 @@ object Constants {
     const val IS_SSL_BOOL = false
     const val TIMEOUT = 1000
 
+
+    //ISO Message constants
     const val TMK_PROCESSING_CODE = "9A0000"
     const val TSK_PROCESSING_CODE = "9B0000"
     const val TPK_PROCESSING_CODE = "9G0000"
     const val DOWNLOAD_PARAM_PROCESSING_CODE = "9C0000"
+
+    const val BALANCE_MTI = "0100"
+    const val PURCHASE_MTI = "0200"
+    const val REVERSAL_MTI = "0420"
+    const val BALANCE_PROCESSING_CODE = "310000"
+    const val PURCHASE_PROCESSING_CODE = "000000"
+//    const val reversalProcessingCode = "0100"
 
     const val SIXTY_FOUR_ZEROS = "0000000000000000000000000000000000000000000000000000000000000000"
 
@@ -37,4 +47,6 @@ object Constants {
     const val TERMINAL_CAPABILITY_NO_CVM = "E0DOC8" // Tag : 9F33
     const val TERMINAL_CAPABILITY_CVM_ONLY = "E020C8" // Tag: 9F33 // Signature only
     const val ADDITIONAL_TERMINAL_CAPABILITY = "7F00F0F001" // Tag : 9F40  or // 6000F0A001
+
+
 }
