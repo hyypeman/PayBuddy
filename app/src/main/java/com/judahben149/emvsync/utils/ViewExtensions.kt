26971700da1d0afb2bda08008105fd84be903524
Judah <judahben149@gmail.com>
2023-07-27 23:13:35 +0100

    Added new view related extension functions

--- app/src/main/java/com/judahben149/emvsync/utils/ViewExtensions.kt
+++ app/src/main/java/com/judahben149/emvsync/utils/ViewExtensions.kt
@@ -1,12 +1,17 @@
 package com.judahben149.emvsync.utils
 
 import android.content.Context
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.airbnb.lottie.LottieAnimationView
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.button.MaterialButton
 import com.judahben149.emvsync.R
+import com.judahben149.emvsync.presentation.shared.ConfirmationDialog
+import com.judahben149.emvsync.presentation.shared.ProgressDialog
+import com.judahben149.emvsync.utils.Constants.CONFIRMATION_DIALOG
+import com.judahben149.emvsync.utils.Constants.PROGRESS_DIALOG
 
 fun TextView.successfulColor() {
     this.setTextColor(resources.getColor(R.color.green))
@@ -76,5 +81,57 @@ fun showDialog(
         }
         .setCancelable(isCancellableOnTouchOutside)
         .show()
+}
+
+fun showProgressDialog(childFragmentManager: FragmentManager, dialogText: String) {
+    val dialogFragment = ProgressDialog.newInstance(dialogText)
+    dialogFragment.isCancelable = false
+    dialogFragment.show(childFragmentManager, PROGRESS_DIALOG)
+}
+
+fun hideProgressDialog(childFragmentManager: FragmentManager) {
+    val dialogFragment = childFragmentManager.findFragmentByTag(PROGRESS_DIALOG)
+    if (dialogFragment != null) {
+        (dialogFragment as ProgressDialog).dismiss()
+    }
+}
+
+fun showConfirmationDialog(
+    childFragmentManager: FragmentManager,
+    title: String,
+    body: String,
+    positiveText: String = "OK",
+    negativeText: String = "Cancel",
+    actionPositive: () -> Unit,
+    actionNegative: () -> Unit
+) {
+    val dialogFragment = ConfirmationDialog.newInstance(
+        title,
+        body,
+        positiveText,
+        negativeText,
+        actionPositive,
+        actionNegative
+    )
+    dialogFragment.isCancelable = false
+    dialogFragment.show(childFragmentManager, CONFIRMATION_DIALOG)
+}
+
+fun hideConfirmationDialog(childFragmentManager: FragmentManager) {
+    val dialogFragment = childFragmentManager.findFragmentByTag(CONFIRMATION_DIALOG)
+    if (dialogFragment != null) {
+        (dialogFragment as ConfirmationDialog).dismiss()
+    }
+}
+
+
+fun Button.disableButton() {
+    this.isEnabled = false
+    this.setBackgroundColor(resources.getColor(R.color.grey_light))
+}
+
+fun Button.enableButton() {
+    this.isEnabled = true
+    this.setBackgroundColor(resources.getColor(R.color.deep_brown))
 }
 
